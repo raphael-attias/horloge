eacf16d6b116085657960583c44ab4314bbc229e
raphael.attias <raphael.attias@laplateforme.io>
2023-12-01 11:40:58 +0100

    mise en forme

--- horloge.py
+++ horloge.py
@@ -45,29 +45,29 @@ def choix(choisit):
 
 fenetre = Tk()
 fenetre.resizable(width=False, height=False)
-fenetre.geometry("200x100")
-fenetre.configure(background="gray")
+fenetre.geometry("200x150")
+fenetre.configure(background="#001F3F")
 fenetre.title("Horloge")
 
 Label_temps = Label(fenetre, font=(30), bg='white')
 Label_temps.grid(row=1, column=1, columnspan=3)
 
-btn24 = Button(fenetre, text="form 24h", command=lambda: choix(True))
+btn24 = Button(fenetre, text="Fonct 24h", command=lambda: choix(True))
 btn24.grid(row=2, column=1, columnspan=1)
 
-btn12 = Button(fenetre, text="form 12h", command=lambda: choix(False))
+btn12 = Button(fenetre, text="Fonct 12h", command=lambda: choix(False))
 btn12.grid(row=2, column=2, columnspan=1)
 
-btnalarm = Button(fenetre, text="alarme", command=lambda: alarme())
+btnalarm = Button(fenetre, text="Alarme", command=lambda: alarme())
 btnalarm.grid(row=2, column=3, columnspan=1)
 
-btn_pause = Button(fenetre, text="Pause/Resume", command=pause_resume)
+btn_pause = Button(fenetre, text="Pause/Reprendre", command=pause_resume)
 btn_pause.grid(row=3, column=1, columnspan=3)
 
 saisieheure = Entry(fenetre, bd=2, width=9, textvariable=alarmheure)
 saisieheure.grid(row=4, column=1)
 
-Label_alarmhp = Label(fenetre, font=("", 8), bg='white', text="Heure | Min")
+Label_alarmhp = Label(fenetre, font=("", 8), bg='grey', text="Heure | Min")
 Label_alarmhp.grid(row=4, column=2)
 
 saisiemin = Entry(fenetre, bd=2, width=9, textvariable=alarmmin)
