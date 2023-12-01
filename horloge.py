8cc0813218ea07f04641e2b0ca65da9a041ee479
raphael.attias <raphael.attias@laplateforme.io>
2023-12-01 09:42:06 +0100

    test 2.3

--- horloge.py
+++ horloge.py
@@ -1,50 +1,77 @@
-from tkinter import *			
+from tkinter import *
 from time import *
 
-#variables
-vartype=True
-alarmheure=""
-alarmmin=""
+# variables
+vartype = True
+alarmheure = ""
+alarmmin = ""
+pause_status = False  
+last_known_time = ""
+
 def alarme():
     global alarmheure, alarmmin
-    alarmheure= saisieheure.get()
-    alarmmin= saisiemin.get()
+    alarmheure = saisieheure.get()
+    alarmmin = saisiemin.get()
+
+def pause_resume():
+    global pause_status, last_known_time
+    pause_status = not pause_status
+    if not pause_status:
+        last_known_time = strftime("%H:%M:%S")
+    else:
+        last_known_time = ""
+
 def afficher_heure():
-    if(alarmheure==strftime("%H") and alarmmin==strftime("%M")):
-        alarm=True
-    else:
-        alarm=False
-    if alarm == True:
-        Label_temps.config(text="|| Alarme ||")
-    else:
-        if vartype==True:
-            Label_temps.config(text=strftime("%H:%M:%S"))
+    if not pause_status:  
+        if alarmheure == strftime("%H") and alarmmin == strftime("%M"):
+            alarm = True
         else:
-            Label_temps.config(text=strftime("%I:%M %p"))
+            alarm = False
+        if alarm:
+            Label_temps.config(text="|| Alarme ||")
+        else:
+            if vartype:
+                Label_temps.config(text=strftime("%H:%M:%S"))
+            else:
+                Label_temps.config(text=strftime("%I:%M %p"))
+    else:
+        Label_temps.config(text=last_known_time) 
+
     Label_temps.after(200, afficher_heure)
+
 def choix(choisit):
     global vartype
     vartype = choisit
 
-
 fenetre = Tk()
-fenetre.resizable(width=False, height=False) 
+fenetre.resizable(width=False, height=False)
 fenetre.geometry("200x100")
 fenetre.configure(background="gray")
 fenetre.title("Horloge")
-Label_temps = Label(fenetre, font=(30), bg='white') 
+
+Label_temps = Label(fenetre, font=(30), bg='white')
 Label_temps.grid(row=1, column=1, columnspan=3)
-btn24 = Button(fenetre, text ="form 24h", command = lambda: choix(True))
+
+btn24 = Button(fenetre, text="form 24h", command=lambda: choix(True))
 btn24.grid(row=2, column=1, columnspan=1)
-btn12 = Button(fenetre, text ="form 12h", command = lambda: choix(False))
+
+btn12 = Button(fenetre, text="form 12h", command=lambda: choix(False))
 btn12.grid(row=2, column=2, columnspan=1)
-btnalarm = Button(fenetre, text ="alarme", command = lambda: alarme())
+
+btnalarm = Button(fenetre, text="alarme", command=lambda: alarme())
 btnalarm.grid(row=2, column=3, columnspan=1)
-saisieheure = Entry(fenetre, bd = 2, width=9, textvariable=alarmheure)
-saisieheure.grid(row=3, column=1)
-Label_alarmhp = Label(fenetre, font=("",8), bg='white',text="Heure | Min") 
-Label_alarmhp.grid(row=3, column=2)
-saisiemin = Entry(fenetre, bd = 2, width=9,textvariable=alarmmin)
-saisiemin.grid(row=3, column=3)
+
+btn_pause = Button(fenetre, text="Pause/Resume", command=pause_resume)
+btn_pause.grid(row=3, column=1, columnspan=3)
+
+saisieheure = Entry(fenetre, bd=2, width=9, textvariable=alarmheure)
+saisieheure.grid(row=4, column=1)
+
+Label_alarmhp = Label(fenetre, font=("", 8), bg='white', text="Heure | Min")
+Label_alarmhp.grid(row=4, column=2)
+
+saisiemin = Entry(fenetre, bd=2, width=9, textvariable=alarmmin)
+saisiemin.grid(row=4, column=3)
+
 afficher_heure()
 fenetre.mainloop()
