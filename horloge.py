e5d853aa807553f85bfd12f3230b7e1848a9c4e2
raphael.attias <raphael.attias@laplateforme.io>
2023-12-20 11:52:12 +0100

    update

--- horloge.py
+++ horloge.py
@@ -1,24 +1,15 @@
-# -*- coding: utf-8 -*-
-"""
-Made in Marseille
-
-@author: Raphael
-"""
-#programme en-tete programme python
-#version python : 
-#auteur : ATTAS Raphaël
-#email : raphael.attias@laplateforme.io
-
-
 from tkinter import *
 from time import *
 
-# les variables
 vartype = True
 alarmheure = ""
 alarmmin = ""
-pause_status = False  
+pause_status = False
 last_known_time = ""
+
+def choix(choisit):
+    global vartype
+    vartype = choisit
 
 def alarme():
     global alarmheure, alarmmin
@@ -33,39 +24,59 @@ def pause_resume():
     else:
         last_known_time = ""
 
+def afficher_heure_tuple(heure_tuple):
+    global vartype
+
+    if len(heure_tuple) >= 6:
+        heures, minutes, secondes = heure_tuple[3:6]
+    else:
+        heures, minutes, secondes = heure_tuple[0:3]
+
+    if vartype:
+        Label_temps.config(text=f"{heures:02d}:{minutes:02d}:{secondes:02d}")
+    else:
+        am_pm = "am" if heures < 12 else "pm"
+        heures_12h_format = heures % 12 if heures % 12 != 0 else 12
+        Label_temps.config(text=f"{heures_12h_format:02d}:{minutes:02d}:{secondes:02d} {am_pm}")
+
+def afficher_heure_manuelle(heure_str):
+    heure_tuple = strptime(heure_str, "%H:%M:%S")
+    afficher_heure_tuple(heure_tuple)
+
+def regler_heure_manuelle():
+    heure_manuelle = heure_manuelle_entry.get()
+    afficher_heure_manuelle(heure_manuelle)
+
 def afficher_heure():
-    if not pause_status:  
-        if alarmheure == strftime("%H") and alarmmin == strftime("%M"):
+    global alarmheure, alarmmin, pause_status, last_known_time
+
+    if not pause_status:
+        current_time_tuple = localtime()
+        if (alarmheure, alarmmin) == (strftime("%H"), strftime("%M")):
             alarm = True
         else:
             alarm = False
         if alarm:
             Label_temps.config(text="|| Alarme ||")
         else:
-            if vartype:
-                Label_temps.config(text=strftime("%H:%M:%S"))
-            else:
-                Label_temps.config(text=strftime("%I:%M %p"))
+            afficher_heure_tuple(current_time_tuple)
     else:
-        Label_temps.config(text=last_known_time) 
+        afficher_heure_tuple(strptime(last_known_time, "%H:%M:%S"))
 
-    Label_temps.after(200, afficher_heure)
+    if heure_manuelle_entry.get():
+        afficher_heure_manuelle(heure_manuelle_entry.get())
 
-def choix(choisit):
-    global vartype
-    vartype = choisit
+    fenetre.after(1000, afficher_heure)  # Planifier l'appel à afficher_heure après 1000 ms (1 seconde)
 
-#def fenêtre
 fenetre = Tk()
 fenetre.resizable(width=False, height=False)
-fenetre.geometry("200x150")
+fenetre.geometry("200x200")
 fenetre.configure(background="#001F3F")
 fenetre.title("Horloge")
 
 Label_temps = Label(fenetre, font=(30), bg='white')
 Label_temps.grid(row=1, column=1, columnspan=3)
 
-# les boutons
 btn24 = Button(fenetre, text="Fonct 24h", command=lambda: choix(True))
 btn24.grid(row=2, column=1, columnspan=1)
 
@@ -87,5 +98,12 @@ Label_alarmhp.grid(row=4, column=2)
 saisiemin = Entry(fenetre, bd=2, width=9, textvariable=alarmmin)
 saisiemin.grid(row=4, column=3)
 
+heure_manuelle_entry = Entry(fenetre, bd=2, width=9)
+heure_manuelle_entry.grid(row=5, column=1)
+
+btn_regler_manuelle = Button(fenetre, text="Régler Manuellement", command=regler_heure_manuelle)
+btn_regler_manuelle.grid(row=5, column=2, columnspan=2)
+
 afficher_heure()
+
 fenetre.mainloop()
